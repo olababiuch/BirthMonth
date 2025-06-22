a22d425a4ea855a3c621092d8a1239e1cca56c08
olababiuch <ola.babiuch7@gmail.com>
2025-06-22 22:52:38 +0200

    task 2 completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public class Main {
+
+        public static void main(String[] args) {
+
+            Scanner in =  new Scanner(System.in);
+            int birthMonth = 0;
+            String trash = "";
+
+            System.out.print("Enter your birth month (1-12): ");
+
+            if(in.hasNextInt()) {
+                birthMonth = in.nextInt();
+                in.nextLine();
+                if (birthMonth >= 1 && birthMonth <= 12) {
+                    System.out.println("Your birthday month is " + birthMonth);
+                } else {
+                    System.out.println("Invalid input: " + birthMonth + ". The number should be between 1-12");
+                }
+            } else  {
+                trash = in.nextLine();
+                System.out.println("Invalid input: " + trash + ". Enter a number between 1-12");
+
+
+                }
+
+            }
         }
-    }
-}
